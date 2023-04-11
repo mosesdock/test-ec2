3ae0c61af8b04dde4120faf9ad29513f99e507c5
Idan.dev <idanmoshe16@gmail.com>
2023-04-11 16:34:52 +0300

    update main.tf file

--- main.tf
+++ main.tf
@@ -124,5 +124,8 @@ resource "aws_lb_target_group_attachment" "web_tga" {
 
 resource "aws_security_group_rule" "web_lb_ingress" {
   type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
   security_group_id = aws_security_group.web_sg.id
 }
